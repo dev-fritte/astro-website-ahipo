de6b9d0f911f7abb48a0fd4c7f1ca58e4898356a
Patrick Schmidt <p.schmidt@goconut.com>
2025-01-12 00:16:39 +0100

    added some footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,18 +7,28 @@ export const Footer: React.FC = () => {
     return (
         <footer className={'py-4 w-full bottom-0'}>
             <ContentBlock>
-                <div className={'flex flex-col gap-4'}>
+                <div className={'flex-1 flex flex-col gap-10'}>
                     <div className={'w-56'}>
                         <Logo/>
                     </div>
 
-                    <div className="flex justify-between">
+                    <div className="flex-1 flex justify-between">
                         <div>
                             <p>{'Rechtsanwälting und rechtliche Betreuerin'}</p>
                             <p>{'Melanie Ahipo'}</p>
                             <p>{'Musterstraße 1A'}</p>
                             <p>{'12345 Musterstadt'}</p>
                         </div>
+
+                        <div>
+                            <p>{'Folgen Sie mir auf Social Media'}</p>
+                        </div>
+                    </div>
+
+                    <div className={'flex gap-4 text-sm'}>
+                        <a href={'/imprint'}>{'Impressum'}</a>
+                        <div className="inline-block w-0.5 self-stretch bg-corporate-main"></div>
+                        <a href={'/privacy'}>{'Datenschutzerklärung'}</a>
                     </div>
                 </div>
             </ContentBlock>
